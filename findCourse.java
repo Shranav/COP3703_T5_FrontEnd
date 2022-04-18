642e9fb4b0f7a29bac7eaab90627262937f3ce73
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2022-04-18 15:38:48 -0400

    Finding the courses offered with department name or code

---
+++ findCourse.java
@@ -0,0 +1,71 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Button;
+
+public class findCourse {
+
+	protected Shell cFindWindow;
+	private Text text;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			findCourse window = new findCourse();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		cFindWindow.open();
+		cFindWindow.layout();
+		while (!cFindWindow.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		cFindWindow = new Shell();
+		cFindWindow.setSize(450, 300);
+		cFindWindow.setText("Courses Offered");
+		
+		Label lblCourseOffered = new Label(cFindWindow, SWT.NONE);
+		lblCourseOffered.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
+		lblCourseOffered.setBounds(136, 10, 158, 21);
+		lblCourseOffered.setText("Find Course Offered");
+		
+		text = new Text(cFindWindow, SWT.BORDER);
+		text.setBounds(190, 37, 136, 21);
+		
+		Label lblNewLabel_1 = new Label(cFindWindow, SWT.NONE);
+		lblNewLabel_1.setBounds(10, 37, 174, 15);
+		lblNewLabel_1.setText("Insert department name or code:");
+		
+		List list = new List(cFindWindow, SWT.BORDER);
+		list.setBounds(97, 73, 250, 153);
+		
+		Button btnNewButton = new Button(cFindWindow, SWT.NONE);
+		btnNewButton.setBounds(10, 228, 75, 25);
+		btnNewButton.setText("Back");
+
+	}
+}
