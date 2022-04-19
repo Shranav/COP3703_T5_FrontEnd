e7bb4516961c892ccc1ccf1455f5963241e7c922
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2022-04-18 23:33:53 -0400

    Added Department Info

--- addDepartment.java
+++ addDepartment.java
@@ -2,13 +2,15 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 public class addDepartment {
 
-	protected Shell dWindow;
+	protected Shell shell;
 	private Text dNameTxt;
 	private Label code;
 	private Text codeTxt;
@@ -38,12 +40,22 @@ public class addDepartment {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		dWindow.open();
-		dWindow.layout();
-		while (!dWindow.isDisposed()) {
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
+		}
+	}
+	
+	public static void openDepartment() {
+		
+		try {
+			addDepartment window = new addDepartment();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -51,57 +63,77 @@ public class addDepartment {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		dWindow = new Shell();
-		dWindow.setSize(346, 358);
-		dWindow.setText("Department");
+		shell = new Shell();
+		shell.setSize(346, 358);
+		shell.setText("Department");
 		
-		Label dName = new Label(dWindow, SWT.NONE);
+		Label dName = new Label(shell, SWT.NONE);
 		dName.setText("Department Name:");
 		dName.setBounds(10, 64, 107, 15);
 		
-		dNameTxt = new Text(dWindow, SWT.BORDER);
+		dNameTxt = new Text(shell, SWT.BORDER);
 		dNameTxt.setBounds(123, 61, 76, 21);
 		
-		Label lblDepartment = new Label(dWindow, SWT.NONE);
-		lblDepartment.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
-		lblDepartment.setBounds(98, 10, 162, 21);
-		lblDepartment.setText("Add Department Info");
+		Label Department = new Label(shell, SWT.NONE);
+		Department.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.BOLD));
+		Department.setBounds(63, 10, 199, 25);
+		Department.setText("Add Department Info");
 		
-		code = new Label(dWindow, SWT.NONE);
+		code = new Label(shell, SWT.NONE);
 		code.setText("Code:");
 		code.setBounds(10, 100, 36, 15);
 		
-		codeTxt = new Text(dWindow, SWT.BORDER);
+		codeTxt = new Text(shell, SWT.BORDER);
 		codeTxt.setBounds(52, 97, 76, 21);
 		
-		officeNum = new Label(dWindow, SWT.NONE);
+		officeNum = new Label(shell, SWT.NONE);
 		officeNum.setBounds(10, 146, 82, 15);
 		officeNum.setText("Office Number:");
 		
-		officeNumTxt = new Text(dWindow, SWT.BORDER);
+		officeNumTxt = new Text(shell, SWT.BORDER);
 		officeNumTxt.setBounds(98, 143, 76, 21);
 		
-		college = new Label(dWindow, SWT.NONE);
+		college = new Label(shell, SWT.NONE);
 		college.setBounds(10, 185, 49, 15);
 		college.setText("College:");
 		
-		collegeTxt = new Text(dWindow, SWT.BORDER);
+		collegeTxt = new Text(shell, SWT.BORDER);
 		collegeTxt.setBounds(63, 182, 76, 21);
 		
-		officePhone = new Label(dWindow, SWT.NONE);
+		officePhone = new Label(shell, SWT.NONE);
 		officePhone.setBounds(10, 232, 77, 15);
 		officePhone.setText("Office Phone:");
 		
-		officePhoneTxt = new Text(dWindow, SWT.BORDER);
+		officePhoneTxt = new Text(shell, SWT.BORDER);
 		officePhoneTxt.setBounds(93, 229, 76, 21);
 		
-		Button backButton = new Button(dWindow, SWT.NONE);
-		backButton.setBounds(10, 289, 75, 25);
-		backButton.setText("Back");
+		Button btnBack = new Button(shell, SWT.NONE);
+		btnBack.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
 		
-		Button subButton = new Button(dWindow, SWT.NONE);
-		subButton.setBounds(253, 289, 75, 25);
-		subButton.setText("Submit");
+				// Back to menu and close current one.
+				shell.close();
+				menuScreen.openMenu();
+			}
+		});
+		btnBack.setBounds(10, 289, 75, 25);
+		btnBack.setText("Back");
+		
+		Button btnSub = new Button(shell, SWT.NONE);
+		btnSub.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				//username = txtUsername.getText();
+				//password = txtPassword.getText();
+		
+				// Submit info and return back to menu next screen and close current one.
+				shell.close();
+				menuScreen.openMenu();
+			}
+		});
+		btnSub.setBounds(253, 289, 75, 25);
+		btnSub.setText("Submit");
 
 	}
 }
