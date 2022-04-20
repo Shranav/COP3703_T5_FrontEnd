20f4777080243c5862d41b774a8add984295829a
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2022-04-19 20:49:12 -0400

    Added Department Info

--- addDepartment.java
+++ addDepartment.java
@@ -12,14 +12,20 @@ public class addDepartment {
 
 	protected Shell shell;
 	private Text dNameTxt;
-	private Label code;
+	private Label lblCode;
 	private Text codeTxt;
-	private Label officeNum;
+	private Label lblOfficeNum;
 	private Text officeNumTxt;
-	private Label college;
-	private Text collegeTxt;
-	private Label officePhone;
+	private Label lblOfficePhone;
 	private Text officePhoneTxt;
+	private Label lblCollege;
+	private Text collegeTxt;
+	static String dName;
+	static String code;
+	static String officeNum;
+	static String officePhone;
+	static String college;
+	
 
 	/**
 	 * Launch the application.
@@ -67,9 +73,9 @@ public class addDepartment {
 		shell.setSize(346, 358);
 		shell.setText("Department");
 		
-		Label dName = new Label(shell, SWT.NONE);
-		dName.setText("Department Name:");
-		dName.setBounds(10, 64, 107, 15);
+		Label lblDName = new Label(shell, SWT.NONE);
+		lblDName.setText("Department Name:");
+		lblDName.setBounds(10, 64, 107, 15);
 		
 		dNameTxt = new Text(shell, SWT.BORDER);
 		dNameTxt.setBounds(123, 61, 76, 21);
@@ -79,33 +85,33 @@ public class addDepartment {
 		Department.setBounds(63, 10, 199, 25);
 		Department.setText("Add Department Info");
 		
-		code = new Label(shell, SWT.NONE);
-		code.setText("Code:");
-		code.setBounds(10, 100, 36, 15);
+		lblCode = new Label(shell, SWT.NONE);
+		lblCode.setText("Code:");
+		lblCode.setBounds(10, 100, 36, 15);
 		
 		codeTxt = new Text(shell, SWT.BORDER);
 		codeTxt.setBounds(52, 97, 76, 21);
 		
-		officeNum = new Label(shell, SWT.NONE);
-		officeNum.setBounds(10, 146, 82, 15);
-		officeNum.setText("Office Number:");
+		lblOfficeNum = new Label(shell, SWT.NONE);
+		lblOfficeNum.setBounds(10, 146, 82, 15);
+		lblOfficeNum.setText("Office Number:");
 		
 		officeNumTxt = new Text(shell, SWT.BORDER);
 		officeNumTxt.setBounds(98, 143, 76, 21);
 		
-		college = new Label(shell, SWT.NONE);
-		college.setBounds(10, 185, 49, 15);
-		college.setText("College:");
+		lblCollege = new Label(shell, SWT.NONE);
+		lblCollege.setBounds(10, 219, 49, 15);
+		lblCollege.setText("College:");
 		
 		collegeTxt = new Text(shell, SWT.BORDER);
-		collegeTxt.setBounds(63, 182, 76, 21);
+		collegeTxt.setBounds(63, 216, 76, 21);
 		
-		officePhone = new Label(shell, SWT.NONE);
-		officePhone.setBounds(10, 232, 77, 15);
-		officePhone.setText("Office Phone:");
+		lblOfficePhone = new Label(shell, SWT.NONE);
+		lblOfficePhone.setBounds(10, 182, 77, 15);
+		lblOfficePhone.setText("Office Phone:");
 		
 		officePhoneTxt = new Text(shell, SWT.BORDER);
-		officePhoneTxt.setBounds(93, 229, 76, 21);
+		officePhoneTxt.setBounds(87, 179, 76, 21);
 		
 		Button btnBack = new Button(shell, SWT.NONE);
 		btnBack.addSelectionListener(new SelectionAdapter() {
@@ -124,8 +130,12 @@ public class addDepartment {
 		btnSub.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				//username = txtUsername.getText();
-				//password = txtPassword.getText();
+				
+				dName = dNameTxt.getText();
+				code = codeTxt.getText();
+				officeNum = officeNumTxt.getText();
+				officePhone = officePhoneTxt.getText();
+			        college = collegeTxt.getText();
 		
 				// Submit info and return back to menu next screen and close current one.
 				shell.close();
