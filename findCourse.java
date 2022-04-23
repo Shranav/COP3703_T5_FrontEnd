f2d79e0354e13630b9ca6bc39a5992a249e01318
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2022-04-23 00:12:06 -0400

    Find Courses Offered

--- findCourse.java
+++ findCourse.java
@@ -11,6 +11,8 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class findCourse {
 
@@ -71,44 +73,6 @@ public class findCourse {
 		lblCourseOffered.setText("Find Course Offered");
 		
 		txtFindCourse = new Text(shell, SWT.BORDER);
-		txtFindCourse.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {  //Search for department name and code 
-				
-				String findCourse = txtFindCourse.getText();
-				
-				jdbcHandler sqlconn = new jdbcHandler(loginScreen.username, loginScreen.password);
-				try {
-					// reminder: add sex later on
-					sqlconn.displayCourse(findCourse);
-					enrollStudent.createMsgBox(shell, "Successful", "The entry was successfully updated.");
-					txtFindCourse.setText("");
-				}catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-					enrollStudent.createMsgBox(shell, "Error", "There was an error with the update. Please try again.");
-				}
-				
-				/*//open connection
-				Connection conn = this.createConn();
-				
-				//running query to check if student is 
-				String query = "Select code FROM COURSE AS C, DEPARTMENT AS D WHERE C.code = D.code";
-				PreparedStatement pstmt = conn.prepareStatement(query);
-				pstmt.setString(1, txtFindCourse.getText());
-				ResultSet rs = pstmt.executeQuery();
-				
-				// try to display courses on list 
-				courseList.setModel(DbUtils.resultSetToTableModel(rs));
-				/*while(rs.next())
-				{
-					txtFindCourse.setText(rs.getString(""));
-				}
-				
-				//close connection
-				closeConn(conn);*/
-			}
-		});
 		txtFindCourse.setBounds(190, 37, 136, 21);
 		
 		Label lblFindCourse = new Label(shell, SWT.NONE);
@@ -138,6 +102,25 @@ public class findCourse {
 		btnBack.setText("Back");
 		
 		Button btnSearch = new Button(shell, SWT.NONE);
+			
+			String findCourse = txtFindCourse.getText();
+            
+			jdbcHandler sqlconn = new jdbcHandler(loginScreen.username, loginScreen.password);
+            try {
+                // reminder: add sex later on
+                ResultSet courses = sqlconn.displayCourse(findCourse);
+                while (courses.next()) {
+                    String result = courses.getString(0) + " " + courses.getString(1);
+                    courseList.add(result);
+                }
+                enrollStudent.createMsgBox(shell, "Successful", "The entry was successfully updated.");
+                txtFindCourse.setText("");
+            }catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                enrollStudent.createMsgBox(shell, "Error", "There was an error with the update. Please try again.");
+            }
+			
 		btnSearch.setBounds(336, 35, 75, 25);
 		btnSearch.setText("Search");
 
